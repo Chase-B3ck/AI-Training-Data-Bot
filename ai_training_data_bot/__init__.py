82ee6b55f1f721590221fe83c59e44ab76b8471d
Chase-B3ck <Chase.b3ck@gmail.com>
2025-10-01 21:55:51 -0500

    Update __init__.py

--- ai_training_data_bot/__init__.py
+++ ai_training_data_bot/__init__.py
@@ -1,1 +1,11 @@
 
+__version__ = "3.4.0"
+__author__ = "Chase Beck"
+__email__ = "chase.b3ck@gmail.com"
+
+
+from .bot import TrainingDataBot
+from .core.logging import get_logger
+from .storage.dataset_exporter import DatasetExporter
+
+__all__ = ["TrainingDataBot", "get_logger", "DatasetExporter"]
